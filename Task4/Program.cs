ff74359baca93f723b7dece680b62da907ac829e
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-08 02:28:58 +0300

    Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+int maximum = number1;
+
+if (number2 > maximum)
+{
+    maximum = number2;
+}
+if (number3 > maximum)
+{
+    maximum = number3;
+}
+Console.WriteLine("Максимальное число");
+    Console.WriteLine(maximum);
