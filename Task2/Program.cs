193e0034ced15472d1b477d485306fbc646b2759
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-08 02:10:30 +0300

    Task2 edited

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿Console.WriteLine("Введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine("Максимальное число");
+    Console.WriteLine(number1);
+}
+else if (number2 > number1)
+{
+    Console.WriteLine("Максимальное число");
+    Console.WriteLine(number2);
+}
