494caca29caa5673608f5d4436c6b3535e1966cf
Christina Buramenskaya <christina.buramenskaya@gmail.com>
2022-08-08 03:33:22 +0300

    Task8

---
+++ Task8/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int x = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= x)
+{
+    if (count % 2 == 0)
+    {
+        Console.WriteLine(count);
+        count = count + 1;
+    }
+    else if (count % 2 != 0)
+    {
+        count = count + 1;
+    }
+}
